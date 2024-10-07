b39e5676765ce44dd7937f323b62d69e134ba596
anony45-everywhere <anony45.everywhere@gmail.com>
2024-10-07 21:25:53 +0530

    Update whiterabbit.py

--- llm/whiterabbit.py
+++ llm/whiterabbit.py
@@ -2,7 +2,7 @@ import httpx
 import asyncio
 import os
 import json
-headers = json.load(os.environ.get("HEADERS"))
+headers = os.environ.get("HEADERS")
 
 # Define your async function
 async def response(message_content, message_id="flTSP0c", enhance_prompt=False, use_functions=False):
